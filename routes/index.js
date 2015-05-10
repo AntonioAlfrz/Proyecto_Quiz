8ceb3a2f6470c5427897d117a0b967a52b0e25d4
Antonio Alférez <aj.alferez@alumnos.upm.es>
2015-05-10 09:04:43 -0700

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res, next) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load);
+router.param('commentId', commentController.load);
 
 // Definición de rutas de sésión
 router.get('/login',sessionController.new);
@@ -34,5 +35,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+	sessionController.loginRequired, commentController.publish);
 
 module.exports = router;

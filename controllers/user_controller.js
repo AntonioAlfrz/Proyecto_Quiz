c90a6e9ed0228799b8d56294941f7b615a3908ab
Antonio Alférez <aj.alferez@alumnos.upm.es>
2015-05-13 09:33:47 -0700

    Roles y permisos

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,18 @@
 var models = require('../models/models.js');
 
+// Admin-> Todo, Usuario -> Sólo propietario
+exports.ownershipRequired = function(req, res, next){
+    var objUser = req.user.id;
+    var logUser = req.session.user.id;
+    var isAdmin = req.session.user.isAdmin;
+    
+    if (isAdmin || objUser === logUser) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+};
+
 // Autoload :id
 exports.load = function(req, res, next, userId) {
 	models.User.find({
